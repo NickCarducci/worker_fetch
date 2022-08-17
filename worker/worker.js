c4f0a9bbd5bdc93813e46e18b717733f430a233e
Nick Carducci <nmcarducci@gmail.com>
2022-08-16 20:37:19 -0400

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -12,8 +12,8 @@ return new Response(JSON.stringify(data), {
 import init from "../pkg/hello_world.js";
 export default {
   async fetch(request, env /*, ctx*/) {
-    const wasm_bindgen = await init("./pkg/hello_world_bg.wasm");
-    const { test } = wasm_bindgen;
+    //const wasm_bindgen = await init("./pkg/hello_world_bg.wasm");
+    const { test } = init;
     await wasm_bindgen(wasm);
     
     const data = await test();
