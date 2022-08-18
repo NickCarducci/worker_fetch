2b7b407929eb66bf907aa1942ccac51c0221a65f
Nick Carducci <nmcarducci@gmail.com>
2022-08-18 14:42:42 -0400

    Update worker.js

--- worker/worker.js
+++ worker/worker.js
@@ -2,21 +2,21 @@
   event.respondWith(handleRequest(event.request));
 })
 
-//Fetch and log a request @param {Request} request
+//Fetch and log a request @ param {Request} request
 async function handleRequest(request) {
-const { test } = wasm_bindgen;await wasm_bindgen(wasm);const data = await test();
-return new Response(JSON.stringify(data), {
-  headers: {'Content-Type': 'application/json;charset=UTF-8'},
-  status: 200,
-});}*/
-import init from "../pkg/hello_world.js";
+  const { app } = wasm_bindgen; await wasm_bindgen(wasm); const data = await app();
+  return new Response(JSON.stringify(data), {
+    headers: { 'Content-Type': 'application/json;charset=UTF-8' },
+    status: 200,
+  });
+}*/
+//import init from "../pkg/hello_world.js";
 export default {
   async fetch(request, env /*, ctx*/) {
-    //const wasm_bindgen = await init("./pkg/hello_world_bg.wasm");
-    const { test } = init;
+    const { app } = wasm_bindgen;
     await wasm_bindgen(wasm);
-    
-    const data = await test();
+
+    const data = await app();
     return new Response(JSON.stringify(data), {
       headers: {
         'Content-Type': 'application/json;charset=UTF-8',
